48f13e8ccd4cfcb7d2283671932be06e37524f44
bailey <kbailey.v@gmail.com>
2015-08-11 15:36:14 -0700

    Updated a bit
    Need to reverse flow of spiral.

--- Challenge #227 [Easy] Square Spirals.py
+++ Challenge #227 [Easy] Square Spirals.py
@@ -1,35 +1,43 @@
-def spiral(n, m = 0, arg1 = 0, arg2 = 0):
+def spiral(n):
     x, y, dx, dy = 0, 0, 0, -1
 
-    count = 1
     for i in range(n**2):
         if abs(x) == abs(y) and [dx,dy] != [1,0] or x>0 and y == 1-x:  
             dx, dy = -dy, dx 
-        
-        tp1, tp2 = pos(n, x, y)
-        if arg1 == tp1 and arg2 == tp2:
-            print "(%d, %d) found at number %d" % (tp1, tp2, count)
-        
-        if count == m and m > 0:
-            print "%d pos = (%d, %d)" % (m, tp1, tp2)
-            
-        count += 1
-        
         yield x, y
         x, y = x+dx, y+dy
 
-def pos(n, x, y):
-    return (n/2+1) + x, (n/2+1) + y
+def makeGrid(n):
+    matrix = [[0 for x in range(n)] for x in range(n)]
 
+    a = 0
+    while a < n:
+        b = 0
+        for d in matrix[a]:
+            matrix[a][b] = (a+1, b+1) 
+            b += 1
+        a += 1
+
+    return matrix
+
+        
 if __name__ == "__main__":
-    print list(spiral(3,8))
-    
+    input1 = "3 8".split()
+
+    magic = int(input1[0])
+    m = makeGrid(int(input1[0]))
+    x = list(spiral(int(input1[0])))
+
+    for i in x:
+        print m[i[0]+(magic/2)][i[1]+(magic/2)]
+
+
 
 '''
 
 3
 8
-
+output:(2,3)
 
 7
 1 1
